443ae49d3aef9710e642b5bbb617531beb8919d8
Andrzej Lenart <122370966+AndrzejLenart@users.noreply.github.com>
2023-01-10 16:59:09 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 my_fruit_list = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick smoe fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.title('My Parents New Healthy Diner')
 
